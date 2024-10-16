1c704c74ae50cd37433a3b5322b62acdf0b44919
minimatime <153239144+minimatime@users.noreply.github.com>
2024-10-16 15:27:42 -0400

    Update code.js from glitch

--- code.js
+++ code.js
@@ -1,79 +1,57 @@
 
-// gets a random integer between the min and max
-function getRandomInt(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1)) + min;
+var SPREADSHEET_ID = "1d8vWa0DH3_U91bnHHys6ephanFJXggH1KrsVn1BmhXI";
+var TAB_NAME = "Responses";
+
+/* 
+These are two variables (containers for data) that you need to replace with your own information.
+
+1. SPREADSHEET_ID: To get your spreadsheet ID, hit Share at the top-right and make sure ANYONE WITH THE LINK CAN VIEW. Then, copy the end of your URL in your address bar after docs.google.com/spreadsheets/d/...
+  
+  e.g.
+https://docs.google.com/spreadsheets/d/1gLyh6gv41vl9H1fBmjFrGfKip9dKkvIu6o8sccTw2lY/edit#gid=1875797309
+  copy
+  "1gLyh6gv41vl9H1fBmjFrGfKip9dKkvIu6o8sccTw2lY" is your spreadsheet ID.
+  
+  2. TAB_NAME is just the name of your spreadsheet tab. Write it as it is (it's case-sensitive!). 
+    –– The default Google Sheet tab is Sheet1.
+    –– This also accepts the tab number, in order
+  
+*/
+
+/* 
+   Link to the example spreadsheet: https://docs.google.com/spreadsheets/d/1ndp1b_EgDONxhSEa9rd6N80Y_oEvI57cNbqO9EMUIGQ/edit#gid=0
+*/
+let list = [];
+console.log("pls don't look at this code, she's a bit ugly 🤡 - siri + natasha");
+
+$(document).ready(function () {
+  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID + "/" + TAB_NAME, function (data) {
+     // go over everything in data and run the code below
+    data.forEach(function (entry, index) {
+      if (index == 0) return;
+      list.push(entry);
+      $('#entries').append(`<div class = "yuh"> 
+            <img src='` + getImage(entry.pic) + `'>
+            <h2 class ="name">` + entry.name + entry.emoji + `</h2>
+            <h3 class ="loc">` + entry.location + `</h3>
+            <button style="font-family: monospace;" id="button-` + index + `">` + "click here for feels 💛" +`</button>
+            <p style="display:none" id= "message-` + index + `">` + entry.msg + `</p>
+         </div>`)
+      $("#button-" + index).click(function(){
+            $("#message-" + index).toggle();
+          });
+      
+    });
+  });  
+});
+
+// list.forEach((entry) => document.body.appendChild(createEntryDiv(entry)));
+  
+function getImage(driveURL) {
+  return driveURL.replace("open?", "uc?export=view&");
 }
 
 
 
-var SPREADSHEET_ID_AND_TAB = "1d8vWa0DH3_U91bnHHys6ephanFJXggH1KrsVn1BmhXI/Responses";
 
-
-$(document).ready(function () {
-  
-  
-  $.getJSON("https://opensheet.elk.sh/" + SPREADSHEET_ID_AND_TAB, function (data) {
-    
-    data.forEach(function (row, index) {
-      
-      if(index == 0) return;
-      
-      let div = $(`<div class='cloud' type='` + row.name +`' index="` + index +`">
-        <span class='emoji'>` + row.emoji +`</span>
-        <p>` +  row.msg + `</p>
-        </div>`)
-      .addClass( (row.size).toLowerCase() )
-      .attr("title", row.msg)
-      .css("top", getRandomInt(0, 80) + "%")
-      .css("left", getRandomInt(0, 90) + "%")
-      .appendTo("#clouds");
-  
-    });
-  });  
-});
-  
-
-// Filter
-
-$("#filter").on("click", "button[filter]", function(){
-  
-  $("#filter button[filter]").css("opacity", 0.3);
-  $(this).css("opacity", 1);
-  
-  let to_filter = $(this).attr("filter");
-  console.log(to_filter);
-  
-  if(to_filter == "all"){
-    $("#clouds .cloud").show();
-    return;
-  }
-  
-    $("#clouds .cloud").hide();
-    $("#clouds .cloud[type='" + to_filter +"']").show();
-});
-
-
-// Look at cloud
-$("#clouds").on("click", ".cloud", function(){
-  
-  // get information from cloud
-  let index = $(this).attr("index");
-  let symbol = $(this).find(".emoji").text();
-  let msg = $(this).find("p").text();
-  
-  $("#look .cloud .emoji").text(symbol);
-  $("#look .cloud p").text(msg);
-  
-  $("#look").show();
-  
-  // also, mark that cloud as seen
-  $("#clouds .cloud[index='" + index +"']").attr("seen", "");
-  
-});
-
-// hide when button is clicked
-$("#look").on("click", "button[hide]", function(){
-  $("#look").hide();
-});
+// thank you https://github.com/benborgers/opensheet
